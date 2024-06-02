a1ba25e1dfcfc708773fe48981093b0b316368ce
Gabriel <gabicordoba1998@gmail.com>
2024-06-02 20:18:24 -0300

    Creacion Dominio

---
+++ Dominio/Curso.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+  public class Curso
+    {
+        public int ID { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public string UrlPortada { get; set; }
+    }
+}
